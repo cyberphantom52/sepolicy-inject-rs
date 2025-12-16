9d870f0514a7bc1281de519003b688e2bf503a48
Inam Ul Haq <inam123451@gmail.com>
2025-12-16 23:48:01 +0530

    build: Build libsepol

---
+++ build.rs
@@ -0,0 +1,100 @@
+use std::{env, path::PathBuf};
+
+fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+
+    build_libsepol();
+
+    println!("cargo:rustc-link-lib=static=libsepol");
+}
+
+fn build_libsepol() {
+    let mut libsepol_build = cc::Build::new();
+    let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    let libsepol_dir = manifest_dir.join("selinux").join("libsepol");
+
+    libsepol_build
+        .cpp(false)
+        .include(&libsepol_dir.join("include"))
+        .include(&libsepol_dir.join("cil").join("include"))
+        .include(&libsepol_dir.join("src"))
+        .include(&libsepol_dir.join("cil").join("src"))
+        .flag("-Wno-unused-but-set-variable")
+        .define("HAVE_REALLOCARRAY", None);
+
+    let srcs = [
+        "src/assertion.c",
+        "src/avrule_block.c",
+        "src/avtab.c",
+        "src/boolean_record.c",
+        "src/booleans.c",
+        "src/conditional.c",
+        "src/constraint.c",
+        "src/context.c",
+        "src/context_record.c",
+        "src/debug.c",
+        "src/ebitmap.c",
+        "src/expand.c",
+        "src/handle.c",
+        "src/hashtab.c",
+        "src/hierarchy.c",
+        "src/ibendport_record.c",
+        "src/ibendports.c",
+        "src/ibpkey_record.c",
+        "src/ibpkeys.c",
+        "src/iface_record.c",
+        "src/interfaces.c",
+        "src/kernel_to_cil.c",
+        "src/kernel_to_common.c",
+        "src/kernel_to_conf.c",
+        "src/link.c",
+        "src/mls.c",
+        "src/module.c",
+        "src/module_to_cil.c",
+        "src/node_record.c",
+        "src/nodes.c",
+        "src/optimize.c",
+        "src/polcaps.c",
+        "src/policydb.c",
+        "src/policydb_convert.c",
+        "src/policydb_public.c",
+        "src/policydb_validate.c",
+        "src/port_record.c",
+        "src/ports.c",
+        "src/services.c",
+        "src/sidtab.c",
+        "src/symtab.c",
+        "src/user_record.c",
+        "src/users.c",
+        "src/util.c",
+        "src/write.c",
+        "cil/src/cil.c",
+        "cil/src/cil_binary.c",
+        "cil/src/cil_build_ast.c",
+        "cil/src/cil_copy_ast.c",
+        "cil/src/cil_deny.c",
+        "cil/src/cil_find.c",
+        "cil/src/cil_fqn.c",
+        "cil/src/cil_lexer.c",
+        "cil/src/cil_list.c",
+        "cil/src/cil_log.c",
+        "cil/src/cil_mem.c",
+        "cil/src/cil_parser.c",
+        "cil/src/cil_policy.c",
+        "cil/src/cil_post.c",
+        "cil/src/cil_reset_ast.c",
+        "cil/src/cil_resolve_ast.c",
+        "cil/src/cil_stack.c",
+        "cil/src/cil_strpool.c",
+        "cil/src/cil_symtab.c",
+        "cil/src/cil_tree.c",
+        "cil/src/cil_verify.c",
+        "cil/src/cil_write_ast.c",
+    ];
+
+    for s in &srcs {
+        libsepol_build.file(libsepol_dir.join(s));
+    }
+
+    libsepol_build.compile("libsepol");
+}
